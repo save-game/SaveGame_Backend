86fe118435fff876bc62ee60923a2b4835553196
NavyHubb <sngjae972@gmail.com>
2023-06-06 15:57:20 +0900

    initial commit

---
+++ src/main/java/com/zerototen/savegame/SaveGameApplication.java
@@ -0,0 +1,13 @@
+package com.zerototen.savegame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaveGameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaveGameApplication.class, args);
+    }
+
+}
