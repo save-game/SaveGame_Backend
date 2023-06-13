051bf813c12fb3d874c1fc9e65adabe48efe70e9
KIM <chk7119@naver.com>
2023-06-13 22:27:07 +0900

    fix : ConstraintViolationException 처리를 위한 ExceptionHandler 추가
    - API 테스트에서 ConstraintViolationException 발생 시 예상과 다른 결과 발생 확인
    - ExceptionHandler 추가로 원하는 예상 결과 출력

--- src/main/java/com/zerototen/savegame/exception/ApiExceptionAdvice.java
+++ src/main/java/com/zerototen/savegame/exception/ApiExceptionAdvice.java
@@ -2,6 +2,9 @@ package com.zerototen.savegame.exception;
 
 import java.util.HashMap;
 import java.util.Map;
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -17,6 +20,16 @@ public class ApiExceptionAdvice {
         ex.getBindingResult().getAllErrors()
             .forEach(e -> errors.put(((FieldError) e).getField(), e.getDefaultMessage()));
         return ResponseEntity.badRequest().body(errors);
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<?> handleConstraintViolationError(ConstraintViolationException ex) {
+        Map<String, String> errors = new HashMap<>();
+        for (ConstraintViolation<?> violation : ex.getConstraintViolations()) {
+            String path = violation.getPropertyPath().toString();
+            errors.put(path, violation.getMessage());
+        }
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
