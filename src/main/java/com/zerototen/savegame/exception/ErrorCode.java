d201d39ae6bc9c3815e7982c2c3b912a44f01d77
KIM <chk7119@naver.com>
2023-06-16 04:50:59 +0900

    refactor : CustomException 연동에 따른 CustomException 추가
    - 임시로 RuntimeException으로 처리한 부분을 CustomException으로 처리

--- src/main/java/com/zerototen/savegame/exception/ErrorCode.java
+++ src/main/java/com/zerototen/savegame/exception/ErrorCode.java
@@ -21,7 +21,15 @@ public enum ErrorCode {
     //로그인 & 로그아웃 검증
     JWT_TIMEOUT(HttpStatus.BAD_REQUEST, "만료된 JWT 토큰입니다."),
     LOGIN_CHECK_FAIL(HttpStatus.BAD_REQUEST, "아이디나 패스워드를 확인해주세요."),
-    NOT_SOCIAL_LOGIN(HttpStatus.BAD_REQUEST, "소셜 로그인 기능을 이용하세요");
+    NOT_SOCIAL_LOGIN(HttpStatus.BAD_REQUEST, "소셜 로그인 기능을 이용하세요"),
+
+    // record
+    NOT_FOUND_RECORD(HttpStatus.BAD_REQUEST, "지출 내역을 찾을 수 없습니다"),
+    NOT_MATCH_MEMBER(HttpStatus.BAD_REQUEST, "해당 내역이 사용자의 내역이 아닙니다"),
+    STARTDATE_AFTER_ENDDATE(HttpStatus.BAD_REQUEST, "조회시작일이 조회종료일 이후입니다"),
+    CATEGORY_IS_NULL(HttpStatus.BAD_REQUEST, "카테고리가 null 입니다"),
+    INVALID_TOTAL(HttpStatus.BAD_REQUEST, "합계가 0 이하 또는 null 입니다");
+
 
     private final HttpStatus httpStatus;
     private final String detail;
