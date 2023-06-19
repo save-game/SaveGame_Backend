9cf254011321cd5b7e699978de4e323d60277fb4
NavyHubb <sngjae972@gmail.com>
2023-06-19 18:30:43 +0900

    refactor: 생성자 추가

--- src/main/java/com/zerototen/savegame/domain/dto/request/DuplicationRequest.java
+++ src/main/java/com/zerototen/savegame/domain/dto/request/DuplicationRequest.java
@@ -3,9 +3,11 @@ package com.zerototen.savegame.domain.dto.request;
 import javax.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class DuplicationRequest {
 
     @NotBlank
