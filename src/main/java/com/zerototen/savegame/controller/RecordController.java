314d4af028dcb01095e396675387313fbabaad8a
KIM <chk7119@naver.com>
2023-06-19 23:01:59 +0900

    refactor : 로그인 연동에 의한 코드 리팩토링
    - 모든 응답을 ResponseDto<?> 타입로 통일
    - 로그인과 연동으로 인한 코드 리팩토링

--- src/main/java/com/zerototen/savegame/controller/RecordController.java
+++ src/main/java/com/zerototen/savegame/controller/RecordController.java
@@ -1,11 +1,10 @@
 package com.zerototen.savegame.controller;
 
-import com.zerototen.savegame.domain.dto.CreateRecordForm;
-import com.zerototen.savegame.domain.dto.RecordAnalysisResponse;
-import com.zerototen.savegame.domain.dto.response.RecordResponse;
-import com.zerototen.savegame.domain.dto.UpdateRecordForm;
+import com.zerototen.savegame.domain.dto.request.CreateRecordRequest;
+import com.zerototen.savegame.domain.dto.request.UpdateRecordRequest;
+import com.zerototen.savegame.domain.dto.response.ResponseDto;
 import com.zerototen.savegame.domain.type.Category;
-import com.zerototen.savegame.domain.type.ResponseCode;
+import com.zerototen.savegame.security.TokenProvider;
 import com.zerototen.savegame.service.RecordService;
 import com.zerototen.savegame.validation.EnumList;
 import java.time.LocalDate;
@@ -14,7 +13,6 @@ import javax.validation.Valid;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,6 +20,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,53 +32,47 @@ import org.springframework.web.bind.annotation.RestController;
 public class RecordController {
 
     private final RecordService recordService;
+    private final TokenProvider tokenProvider;
 
     @PostMapping
-    public ResponseEntity<?> createRecord(
-//        @RequestHeader TODO: 토큰에 따라 ID 추출방식 바뀔 예정, 추가 시 memberId 부분을 이걸로 변경
-        @RequestParam Long memberId,
-        @RequestBody @Valid CreateRecordForm form) {
-        recordService.create(form.toServiceDto(memberId));
-        return ResponseEntity.status(ResponseCode.CREATE_SUCCESS.getStatus())
-            .body(ResponseCode.CREATE_SUCCESS.getMessage());
+    public ResponseDto<?> createRecord(
+        @RequestHeader(name = "Authorization") String accessToken,
+        @RequestBody @Valid CreateRecordRequest request) {
+
+        return recordService.create(request.toServiceDto(tokenProvider.getMemberIdByToken(accessToken)));
     }
 
     @GetMapping
-    public ResponseEntity<List<RecordResponse>> getInfos(
-//        @RequestHeader TODO: 토큰에 따라 ID 추출방식 바뀔 예정, 추가 시 memberId 부분을 이걸로 변경
+    public ResponseDto<?> getInfos(
+        @RequestHeader(name = "Authorization") String accessToken,
         @RequestParam LocalDate startDate, @RequestParam LocalDate endDate,
-        @RequestParam(required = false) @Validated @EnumList(enumClass = Category.class, ignoreCase = true) List<String> categories,
-        @RequestParam Long memberId) {
-        return ResponseEntity.ok(recordService.getInfos(memberId, startDate, endDate, categories));
+        @RequestParam(required = false) @Validated @EnumList(enumClass = Category.class, ignoreCase = true)
+        List<String> categories) {
+        return recordService.getInfos(tokenProvider.getMemberIdByToken(accessToken), startDate, endDate, categories);
     }
 
     @GetMapping("/analysis")
-    public ResponseEntity<List<RecordAnalysisResponse>> getAnalysisInfo(
-//        @RequestHeader TODO: 토큰에 따라 ID 추출방식 바뀔 예정, 추가 시 memberId 부분을 이걸로 변경
-        @RequestParam int year, @RequestParam @Valid @Min(1) @Max(12) int month,
-        @RequestParam Long memberId) {
-        return ResponseEntity.ok(recordService.getAnalysisInfo(memberId, year, month));
+    public ResponseDto<?> getAnalysisInfo(
+        @RequestHeader(name = "Authorization") String accessToken,
+        @RequestParam int year, @RequestParam @Valid @Min(1) @Max(12) int month) {
+        return recordService.getAnalysisInfo(tokenProvider.getMemberIdByToken(accessToken), year, month);
     }
 
     @PutMapping("/{recordId}")
-    public ResponseEntity<?> updateRecord(
-//        @RequestHeader TODO: 토큰에 따라 ID 추출방식 바뀔 예정, 추가 시 memberId 부분을 이걸로 변경
+    public ResponseDto<?> updateRecord(
+        @RequestHeader(name = "Authorization") String accessToken,
         @PathVariable Long recordId,
-        @RequestParam Long memberId,
-        @RequestBody @Valid UpdateRecordForm form) {
-        recordService.update(form.toServiceDto(recordId, memberId));
-        return ResponseEntity.status(ResponseCode.UPDATE_SUCCESS.getStatus())
-            .body(ResponseCode.UPDATE_SUCCESS.getMessage());
+        @RequestBody @Valid UpdateRecordRequest request) {
+
+        return recordService.update(request.toServiceDto(recordId, tokenProvider.getMemberIdByToken(accessToken)));
     }
 
     @DeleteMapping("/{recordId}")
-    public ResponseEntity<?> deleteRecord(
-//        @RequestHeader TODO: 토큰에 따라 ID 추출방식 바뀔 예정, 추가 시 memberId 부분을 이걸로 변경
-        @PathVariable Long recordId,
-        @RequestParam Long memberId) {
-        recordService.delete(recordId, memberId);
-        return ResponseEntity.status(ResponseCode.DELETE_SUCCESS.getStatus())
-            .body(ResponseCode.DELETE_SUCCESS.getMessage());
+    public ResponseDto<?> deleteRecord(
+        @RequestHeader(name = "Authorization") String accessToken,
+        @PathVariable Long recordId) {
+
+        return recordService.delete(recordId, tokenProvider.getMemberIdByToken(accessToken));
     }
 
 }
