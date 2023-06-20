9f1d3e57e240d92a29f8af49e0c671ff7ffe9d91
NavyHubb <sngjae972@gmail.com>
2023-06-21 00:38:00 +0900

    docs: Swagger 상의 Contact 삭제

--- src/main/java/com/zerototen/savegame/config/SwaggerConfig.java
+++ src/main/java/com/zerototen/savegame/config/SwaggerConfig.java
@@ -9,7 +9,6 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -47,7 +46,6 @@ public class SwaggerConfig {
         return new ApiInfoBuilder()
             .title("API")
             .description("[SaveGame] REST API")
-            .contact(new Contact("[SaveGame Swagger]", "https://github.com/", "@gmail.com"))
             .version("1.0")
             .build();
     }
