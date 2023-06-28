8866caec5ddf3567288a9c4746cff2ed25c333a4
^_______^ <chk7119@naver.com>
2023-06-29 06:55:42 +0900

    refactor: url 네이밍 규칙 변경에 따른 수정
    - 동사보다 명사
    - 단수보다 복수

--- src/main/java/com/zerototen/savegame/controller/MemberController.java
+++ src/main/java/com/zerototen/savegame/controller/MemberController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/member")
+@RequestMapping("/members")
 public class MemberController {
 
     private final MemberService memberService;
