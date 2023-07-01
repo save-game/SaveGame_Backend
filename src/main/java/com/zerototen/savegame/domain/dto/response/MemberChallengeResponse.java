46bfc43b46f18134828833421d0cc906f22b0b5f
KIM <chk7119@naver.com>
2023-07-02 00:04:57 +0900

    fix : 멤버 챌린지 조회 기능 수정
    - 페이지당 3개씩 페이징 처리
    - 종료된 챌린지도 조회
    - validation 적용

--- src/main/java/com/zerototen/savegame/domain/dto/response/MemberChallengeResponse.java
+++ src/main/java/com/zerototen/savegame/domain/dto/response/MemberChallengeResponse.java
@@ -1,6 +1,5 @@
 package com.zerototen.savegame.domain.dto.response;
 
-import com.zerototen.savegame.domain.entity.Challenge;
 import java.time.LocalDate;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -14,17 +13,7 @@ import lombok.NoArgsConstructor;
 public class MemberChallengeResponse {
 
     private Long challengeId;
-
     private String title;
-
     private LocalDate endDate;
-
-    public static MemberChallengeResponse from(Challenge challenge) {
-        return MemberChallengeResponse.builder()
-            .challengeId(challenge.getId())
-            .title(challenge.getTitle())
-            .endDate(challenge.getEndDate())
-            .build();
-    }
 
 }
--- src/main/java/com/zerototen/savegame/repository/ChallengeMemberRepositoryImpl.java
+++ src/main/java/com/zerototen/savegame/repository/ChallengeMemberRepositoryImpl.java
@@ -1,14 +1,17 @@
 package com.zerototen.savegame.repository;
 
+import com.querydsl.core.types.Projections;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
-import com.zerototen.savegame.domain.entity.Challenge;
+import com.zerototen.savegame.domain.dto.response.MemberChallengeResponse;
 import com.zerototen.savegame.domain.entity.Member;
+import com.zerototen.savegame.domain.entity.QChallenge;
 import com.zerototen.savegame.domain.entity.QChallengeMember;
-import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -18,20 +21,34 @@ public class ChallengeMemberRepositoryImpl implements ChallengeMemberRepositoryC
     private final JPAQueryFactory queryFactory;
 
     @Override
-    public List<Challenge> findChallengeListByMemberOrderByEndDate(Member member) {
+    public Page<MemberChallengeResponse> findChallengeListByMemberOrderByEndDate(Member member,
+        Pageable pageable) {
 
         QChallengeMember challengeMember = QChallengeMember.challengeMember;
 
-        BooleanExpression condition = challengeMember.member.eq(member)
-            .and(challengeMember.challenge.endDate.after(
-                LocalDate.now().minusDays(1))); // 종료된 챌린지는 조회하지 않음
+        QChallenge challenge = challengeMember.challenge;
 
-        return new ArrayList<>(queryFactory
-            .select(challengeMember.challenge)
+        BooleanExpression condition = challengeMember.member.eq(member);
+
+        List<MemberChallengeResponse> result = queryFactory
+            .select(Projections.constructor(MemberChallengeResponse.class,
+                challenge.id,
+                challenge.title,
+                challenge.endDate))
             .from(challengeMember)
             .where(condition)
-            .orderBy(challengeMember.challenge.endDate.asc())
-            .fetch());
+            .orderBy(challenge.endDate.asc())
+            .offset(pageable.getOffset())
+            .limit(pageable.getPageSize())
+            .fetch();
+
+        Long totalCount = queryFactory
+            .select(challenge.id.count())
+            .from(challengeMember)
+            .where(condition)
+            .fetchOne();
+
+        return new PageImpl<>(result, pageable, totalCount != null ? totalCount : 0L);
     }
 
 }
